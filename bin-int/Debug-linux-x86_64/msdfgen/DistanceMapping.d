9713ba6eb7e903d0d87168309af1bf05638797da
Cats Studio <79156677+iojuedioe9rd@users.noreply.github.com>
2026-05-03 20:43:59 +0100

    dfzdfdfdsf

---
+++ bin-int/Debug-linux-x86_64/msdfgen/DistanceMapping.d
@@ -0,0 +1,35 @@
+bin-int/Debug-linux-x86_64/msdfgen/DistanceMapping.o: \
+ core/DistanceMapping.cpp /usr/include/stdc-predef.h \
+ core/DistanceMapping.h core/Range.hpp core/base.h \
+ include/msdfgen/msdfgen-config.h /usr/include/c++/13/cstddef \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h
+/usr/include/stdc-predef.h:
+core/DistanceMapping.h:
+core/Range.hpp:
+core/base.h:
+include/msdfgen/msdfgen-config.h:
+/usr/include/c++/13/cstddef:
+/usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h:
+/usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h:
+/usr/include/features.h:
+/usr/include/features-time64.h:
+/usr/include/x86_64-linux-gnu/bits/wordsize.h:
+/usr/include/x86_64-linux-gnu/bits/timesize.h:
+/usr/include/x86_64-linux-gnu/sys/cdefs.h:
+/usr/include/x86_64-linux-gnu/bits/long-double.h:
+/usr/include/x86_64-linux-gnu/gnu/stubs.h:
+/usr/include/x86_64-linux-gnu/gnu/stubs-64.h:
+/usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h:
+/usr/include/c++/13/pstl/pstl_config.h:
+/usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h:
